858b0e459de3e7ff755cb03a740c6cb24fc89657
Oleg Egorov <danteawake88@gmail.com>
2026-04-18 12:44:59 +0400

    AddToCardTest LocatorTest

---
+++ src/test/java/LocatorTest.java
@@ -0,0 +1,82 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class LocatorTest {
+    private final By usernameFieldLocator = By.id("user-name");
+    private final By passwordFieldLocator = By.id("password");
+    private final By loginButtonLocator = By.id("login-button");
+
+    private final String login = "standard_user";
+    private final String password = "secret_sauce";
+
+    @Test
+    public void checkLocator() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable_notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(usernameFieldLocator).sendKeys(login);
+        driver.findElement(passwordFieldLocator).sendKeys(password);
+        driver.findElement(loginButtonLocator).click();
+
+        //Практика
+        // id
+        By addToCartBackpack = By.id("add-to-cart-sauce-labs-backpack");
+
+        // name
+        By sortContainer = By.name("product_sort_container");
+
+        // classname
+        By itemName = By.className("inventory_item_name");
+
+        // tagname
+        By button = By.tagName("button");
+
+        // linktext
+        By twitterLink = By.linkText("Twitter");
+
+        // partiallinktext
+        By partialTwitter = By.partialLinkText("Twit");
+
+        // XPath
+        By xpathByAttribute = By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']");
+        By xpathByText = By.xpath("//div[text()='Sauce Labs Backpack']");
+        By xpathContainsAttribute = By.xpath("//button[contains(@id,'add-to-cart')]");
+        By xpathContainsText = By.xpath("//div[contains(text(),'Sauce Labs')]");
+        By xpathAncestor = By.xpath("//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item']");
+        By xpathDescendant = By.xpath("//div[@class='inventory_list']/descendant::button");
+        By xpathFollowing = By.xpath("//div[text()='Sauce Labs Backpack']/following::button");
+        By xpathParent = By.xpath("//div[text()='Sauce Labs Backpack']/parent::div");
+        By xpathPreceding = By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']/preceding::div[@class='inventory_item_name']");
+        By xpathAnd = By.xpath("//button[@id='add-to-cart-sauce-labs-backpack' and contains(@class,'btn')]");
+
+        // CSS
+        By cssClass = By.cssSelector(".inventory_item_name");
+        By cssClassDown = By.cssSelector(".btn.btn_primary");
+        By cssClassUp = By.cssSelector(".inventory_list .inventory_item_name");
+        By cssId = By.cssSelector("#add-to-cart-sauce-labs-backpack");
+        By cssTagname = By.cssSelector("button");
+        By cssTagnameClass = By.cssSelector("button.btn_primary");
+        By cssAttributeEquals = By.cssSelector("button[id='add-to-cart-sauce-labs-backpack']");
+        By cssAttributeTilde = By.cssSelector("button[class~='btn']");
+        By cssAttributePipe = By.cssSelector("div[id|='inventory']");
+        By cssAttributeCaret = By.cssSelector("button[id^='add-to-cart']");
+        By cssAttributeDollar = By.cssSelector("button[id$='backpack']");
+        By cssAttributeAsterisk = By.cssSelector("button[id*='add-to-cart']");
+
+        driver.quit();
+    }
+}
