4c295311acd89aa8efc6fe13ad4b322035432319
Oleg Egorov <danteawake88@gmail.com>
2026-05-23 14:37:35 +0400

    PropertyReader + config

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.*;
 import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductsPage;
+import utils.PropertyReader;
 import utils.TestListener;
 
 import java.util.HashMap;
@@ -24,8 +25,8 @@ public class BaseTest {
     protected ProductsPage productsPage;
     protected CartPage cartPage;
 
-    protected String user = System.getProperty("user");
-    protected String password = System.getProperty("password");
+    protected String user = System.getProperty("user", PropertyReader.getProperty("user"));
+    protected String password = System.getProperty("password",PropertyReader.getProperty("password"));
 
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true, description = "Настройка браузера")
